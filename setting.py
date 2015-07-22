ba40b9e6f4deacd0c6ac035305b0b5c00b048319
William Sang <sang.williams@gmail.com>
2015-07-22 17:44:43 +0800

    调整设置对齐

--- setting.py
+++ setting.py
@@ -3,8 +3,8 @@
 
 #  伪装配置
 headers = {
-"Accept-Encoding":"gzip",
-"Cache-Control": "max-age=0",
+    "Accept-Encoding":"gzip",
+    "Cache-Control": "max-age=0",
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
     "Accept-Language":  "zh-CN,zh;q=0.8,en;q=0.6,en-US;q=0.4,zh-TW;q=0.2",
     "Connection" :  "keep-alive",
